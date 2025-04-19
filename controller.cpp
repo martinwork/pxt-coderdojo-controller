31b773609515b88e4e3be36e26a8ba3f4e02d043
jimd80 <43608762+jimd80@users.noreply.github.com>
2025-04-19 22:47:15 +0200

    Update controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -6,23 +6,14 @@ using namespace pxt;
  */
 //% color=#000000 weight=100
 namespace CoderDojo_Controller {
-
-    bool initialized = false;
-
     //%
     void init() {
-        if (initialized) return;
-
         // set all the buttons on the CoderDojo Controller to actually appear as buttons on the micro:bit
-        new MicroBitButton(getPin(MICROBIT_ID_IO_P15)->name, MICROBIT_ID_IO_P15, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-        new MicroBitButton(getPin(MICROBIT_ID_IO_P16)->name, MICROBIT_ID_IO_P16, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-        new MicroBitButton(getPin(MICROBIT_ID_IO_P13)->name, MICROBIT_ID_IO_P13, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-        new MicroBitButton(getPin(MICROBIT_ID_IO_P14)->name, MICROBIT_ID_IO_P14, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-        new MicroBitButton(getPin(MICROBIT_ID_IO_P2)->name, MICROBIT_ID_IO_P2, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-        //new MicroBitButton(getPin(MICROBIT_ID_IO_P8)->name, MICROBIT_ID_IO_P8, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-        //new MicroBitButton(getPin(MICROBIT_ID_IO_P5)->name, MICROBIT_ID_IO_P5, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-        //new MicroBitButton(getPin(MICROBIT_ID_IO_P11)->name, MICROBIT_ID_IO_P11, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
-
-        initialized = true;
+        new MicroBitButton(getPin(MICROBIT_ID_IO_P15)->name, MICROBIT_ID_IO_P15, MICROBIT_BUTTON_SIMPLE_EVENTS, PullUp);
+        new MicroBitButton(getPin(MICROBIT_ID_IO_P16)->name, MICROBIT_ID_IO_P16, MICROBIT_BUTTON_SIMPLE_EVENTS, PullUp);
+        new MicroBitButton(getPin(MICROBIT_ID_IO_P13)->name, MICROBIT_ID_IO_P13, MICROBIT_BUTTON_SIMPLE_EVENTS, PullUp);
+        new MicroBitButton(getPin(MICROBIT_ID_IO_P14)->name, MICROBIT_ID_IO_P14, MICROBIT_BUTTON_SIMPLE_EVENTS, PullUp);
+        new MicroBitButton(getPin(MICROBIT_ID_IO_P2)->name, MICROBIT_ID_IO_P2, MICROBIT_BUTTON_SIMPLE_EVENTS, PullUp);
+        new MicroBitButton(getPin(MICROBIT_ID_IO_P8)->name, MICROBIT_ID_IO_P8, MICROBIT_BUTTON_SIMPLE_EVENTS, PullUp);
     }
 }
