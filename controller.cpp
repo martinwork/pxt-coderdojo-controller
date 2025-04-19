63ce01283a3e40c56bba6601c4db9b5412cfd08a
jimd80 <43608762+jimd80@users.noreply.github.com>
2025-04-19 23:19:51 +0200

    Update controller.cpp

--- controller.cpp
+++ controller.cpp
@@ -6,14 +6,20 @@ using namespace pxt;
  */
 //% color=#000000 weight=100
 namespace CoderDojo_Controller {
+    bool initialized = false;
+
     //%
     void init() {
-        // set all the buttons on the CoderDojo Controller to actually appear as buttons on the micro:bit
+        if (initialized) return;
+        
+        // enable pull-up and enable button events (A and B are already configured)
         new MicroBitButton(getPin(MICROBIT_ID_IO_P15)->name, MICROBIT_ID_IO_P15, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
         new MicroBitButton(getPin(MICROBIT_ID_IO_P16)->name, MICROBIT_ID_IO_P16, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
         new MicroBitButton(getPin(MICROBIT_ID_IO_P13)->name, MICROBIT_ID_IO_P13, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
         new MicroBitButton(getPin(MICROBIT_ID_IO_P14)->name, MICROBIT_ID_IO_P14, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
         new MicroBitButton(getPin(MICROBIT_ID_IO_P2)->name, MICROBIT_ID_IO_P2, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
         new MicroBitButton(getPin(MICROBIT_ID_IO_P8)->name, MICROBIT_ID_IO_P8, MICROBIT_BUTTON_ALL_EVENTS, PullUp);
+
+        initialized = true;
     }
 }
